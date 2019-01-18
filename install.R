6e5197d01e3d30712f6da21bbf7c7a937bb5c632
outcastgeek <outcastgeek+git@gmail.com>
2019-01-17 21:07:57 -0600

    Updated Configuration

--- install.R
+++
@@ -1,9 +0,0 @@
-# Repositories
-#r = getOption("repos")
-#r["CRAN"] = "http://cran.us.r-project.org"
-#options(repos = r)
-
-# R Dependencies
-install.packages("tidyverse")
-install.packages("reticulate")
-install.packages("r2d3")
